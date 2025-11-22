553bddd86fcc95b48cd70451fd099be544d8b2b8
Samuel Kyletoft <samuel@kyletoft.se>
2025-11-22 21:15:18 +0100

    reference1: Update plotting syntax

--- python/reference1.py
+++ python/reference1.py
@@ -46,10 +46,18 @@ for it in range(nt):
 
 def plot2D(x, y, p):
     fig = pyplot.figure(figsize=(11, 7), dpi=100)
-    ax = fig.gca(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
     X, Y = numpy.meshgrid(x, y)
-    surf = ax.plot_surface(X, Y, p[:], rstride=1, cstride=1, cmap=cm.viridis,
-            linewidth=0, antialiased=False)
+    surf = ax.plot_surface(
+        X,
+        Y,
+        p[:],
+        rstride=1,
+        cstride=1,
+        cmap=cm.viridis,
+        linewidth=0,
+        antialiased=False
+    )
     ax.view_init(30, 225)
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
