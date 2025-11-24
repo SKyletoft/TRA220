f367015fb4f4bd9709ba67a1ea1e95e8dcfe418b
Samuel Kyletoft <samuel@kyletoft.se>
2025-11-24 05:20:17 +0100

    main: Deal with printing other shapes of data

--- python/open-plot.py
+++ python/open-plot.py
@@ -4,10 +4,11 @@ import numpy as np
 import plotly.graph_objects as go
 
 data = np.loadtxt('/tmp/poisson.txt')
-x, y, z = data[:, 0], data[:, 1], data[:, 2]
-X = x.reshape(50, 50)
-Y = y.reshape(50, 50)
-Z = z.reshape(50, 50)
+width, height = int(data[0, 0]), int(data[0, 1])
+x, y, z = data[1:, 0], data[1:, 1], data[1:, 2]
+X = x.reshape(width, height)
+Y = y.reshape(width, height)
+Z = z.reshape(width, height)
 
 fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
 fig.show()
--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,31 @@
 #include <fmt/base.h>
 #include <fmt/ranges.h>
 
+#undef NDEBUG
+
 #include <stdexcept>
+#include <iostream>
+#include <cassert>
+
+static constexpr size_t width = 50;
+static constexpr size_t height = 50;
+
+// #define DIFF
 
 auto main() -> int {
-	try {
-		// What does any of this mean? Who knows
-		auto res = poisson(50, 50, 100, 0, 2, 0, 1);
+	// What does any of this mean? Who knows
+	auto res = poisson(width, height, 100, 0, 2, 0, 1);
 #ifdef DIFF
-		for (auto x : res) {
-			fmt::println("{:.05e}", x);
-		}
+	for (auto x : res) {
+		fmt::println("{:.05e}", x);
+	}
 #else
-		for (std::size_t j = 0; j < 50; ++j) {
-			for (std::size_t i = 0; i < 50; ++i) {
-				auto z = res[j * 50 + i];
-				fmt::println("{}\t{}\t{}", i, j, z);
-			}
+	fmt::println("{}\t{}\t0", width, height);
+	for (size_t j = 0; j < height; ++j) {
+		for (size_t i = 0; i < width; ++i) {
+			auto z = res[j * width + i];
+			fmt::println("{}\t{}\t{}", i, j, z);
 		}
+	}
 #endif
-	}
-	catch (std::runtime_error e) {
-		fmt::println("Uncaught exception: {}", e.what());
-	}
 }
